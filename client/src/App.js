cedcfde26a111efd14a6ba0e0c9270edc993d0a9
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-27 14:47:31 +0100

    Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <main>
+
       <header>
       <a hre='' className='logo'>MyBlog</a>
       <nav>
@@ -10,6 +11,70 @@ function App() {
       <a href=''>Register</a>
       </nav>
       </header>
+
+        <div className='post'>
+
+          <div className='image'>
+          <img src='https://nsf-gov-resources.nsf.gov/styles/hero_small/s3/2023-04/Andromeda_galaxy_h.jpg?VersionId=IHJS5Rc94YcMivFNWBjtgZ8XUOyJKOCe&itok=kjpCxb_a' alt=''></img>
+          </div>
+          
+          <div className='texts'>
+          <h2>Astronomy and Space</h2>
+          <p className='info'>
+            <a className='author'> Omar Zouaoui </a>
+            <time>2023-06-27 14:38</time>
+          </p>
+          <p className='summary'>
+            The discovery of planets orbiting other stars and the search for life on these worlds.
+            Understanding of the origin and evolution of the early universe.
+            Understanding of mysterious dark matter and dark energy, which together comprise 95% of the observable universe.</p>
+          </div> 
+
+        </div>
+
+        
+        <div className='post'>
+          
+          <div className='image'>
+          <img src='https://nsf-gov-resources.nsf.gov/styles/hero_small/s3/2023-04/Andromeda_galaxy_h.jpg?VersionId=IHJS5Rc94YcMivFNWBjtgZ8XUOyJKOCe&itok=kjpCxb_a' alt=''></img>
+          </div>  
+
+          <div className='texts'>
+          <h2>Astronomy and Space</h2>
+          <p className='info'>
+            <a className='author'> Omar Zouaoui </a>
+            <time>2023-06-27 14:38</time>
+          </p>
+          <p className='summary'>
+            The discovery of planets orbiting other stars and the search for life on these worlds.
+            Understanding of the origin and evolution of the early universe.
+            Understanding of mysterious dark matter and dark energy, which together comprise 95% of the observable universe.</p>
+          </div> 
+
+        </div>
+
+        
+        <div className='post'>
+
+        <div className='image'>
+        <img src='https://nsf-gov-resources.nsf.gov/styles/hero_small/s3/2023-04/Andromeda_galaxy_h.jpg?VersionId=IHJS5Rc94YcMivFNWBjtgZ8XUOyJKOCe&itok=kjpCxb_a' alt=''></img>
+        </div>
+
+        <div className='texts'>
+          <h2>Astronomy and Space</h2>
+          <p className='info'>
+            <a className='author'> Omar Zouaoui </a>
+            <time>2023-06-27 14:38</time>
+          </p>
+          <p className='summary'>
+            The discovery of planets orbiting other stars and the search for life on these worlds.
+            Understanding of the origin and evolution of the early universe.
+            Understanding of mysterious dark matter and dark energy, which together comprise 95% of the observable universe.</p>
+          </div> 
+
+        </div>
+
+
     </main>
      );
 }
