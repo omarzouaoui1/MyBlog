04038d4b22c0a7c693487edbe60b504c2709b032
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-29 11:35:29 +0100

    Auth done

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require("cors");
 
 const app  = express();
+const jwt = require('jsonwebtoken');
 
-app.use(cors());
+app.use(cors({credentials: true, origin:'http://localhost:3000'}));
 app.use(express.json());
 
 
@@ -14,6 +15,7 @@ const User = require('./models/User');
 
 
 const saltRounds = bcrypt.genSaltSync(10);
+const secret = 'n5zrfzrfzrf5abiokk75p2lkhjrd';
 
 
 //mongoDB Connection
@@ -25,6 +27,7 @@ mongoose.connect('mongodb+srv://omarzouaoui100:raqiHxlTr56AkLlo@cluster0.ofzbzfd
     console.error('Error connecting to MongoDB:', error);
   });
 
+//Register
 app.post("/register", async (req, res) => {
     const {username, password}=req.body;
     try{
@@ -44,6 +47,22 @@ app.post("/register", async (req, res) => {
         res.status(400).json(e);
     }
 });
+
+//Login
+app.post('/login', async (req, res) => {
+  const {username, password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if(passOk){
+    //logged in
+    jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+      if(err) throw err;
+      res.cookie('token', token).json('ok');
+    });
+  } else {
+    res.status(400).json('wrong password')
+  }
+})
 
 const port = 4000;
 
