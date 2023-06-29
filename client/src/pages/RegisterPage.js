45616a0a668a4992fd96cd4f57855bc7a2e5659f
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-29 10:44:42 +0100

    hashing Password

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -6,11 +6,16 @@ export default function RegisterPage(){
 
     async function register(ev){
         ev.preventDefault();
-        await fetch('http://localhost:4000/register', {
-            method: 'POST',
-            body: JSON.stringify({username, password}),
-            headers: {'Content-Type':'application/json'},
-        })
+            const response = await fetch('http://localhost:4000/register', {
+                    method: 'POST',
+                    body: JSON.stringify({username, password}),
+                    headers: {'Content-Type':'application/json'},
+                });
+        if(response.status === 200){
+            alert('Registration successful')
+        } else {
+            alert('Registration failed')
+        }
     }
 
     return(
--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require("cors");
 
-const mongoose = require('mongoose');
-const User = require('./models/User');
-
 const app  = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+const bcrypt = require('bcryptjs');
+
+const mongoose = require('mongoose');
+const User = require('./models/User');
+
+
+const saltRounds = bcrypt.genSaltSync(10);
+
 
 //mongoDB Connection
 mongoose.connect('mongodb+srv://omarzouaoui100:raqiHxlTr56AkLlo@cluster0.ofzbzfd.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -20,12 +27,22 @@ mongoose.connect('mongodb+srv://omarzouaoui100:raqiHxlTr56AkLlo@cluster0.ofzbzfd
 
 app.post("/register", async (req, res) => {
     const {username, password}=req.body;
-    const userDoc = await User.create({
-        username,
-        password
-    })
-    res.json(userDoc);
-
+    try{
+        const userDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password, saltRounds, (err, hash) => {
+                if(err){
+                  console.log(err)
+                } else{
+                  console.log(hash)
+                }
+              })
+        })
+        res.json(userDoc);
+    } catch(error) {
+      console.log(e)
+        res.status(400).json(e);
+    }
 });
 
 const port = 4000;
