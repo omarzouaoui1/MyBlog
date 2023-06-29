488bd066382489aa2d56a12b0c7e78f631810a38
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-06-29 06:39:57 +0100

    working on Auth

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,9 +1,28 @@
 export default function RegisterPage(){
+    const [username, setUsername] = setState('');
+    const [password, setPassword] = setState('');
+
+    function register(ev){
+        ev.preventDefault();
+        fetch('http://localhost:4000', {
+            method: 'POST',
+            body: JSON.stringify({username, password})
+        })
+    }
+
     return(
-        <form action=" " className="register">
+        <form action=" " className="register" onSubmit={}>
             <h1>Register</h1>
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
+            <input type="text" 
+            placeholder="username" 
+            value={username} 
+            onChange={ev => setUsername(ev.target.value)}
+            />
+            <input type="password" 
+            placeholder="password"
+            value={password}
+            onChange={ev => setPassword(ev.target.value)}
+            />
             <button>Register</button>
         </form>
     )
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app  = express();
 
-app.get("/",(req, res) => {
+app.post("/register",(req, res) => {
     res.json('test ok');
 });
 
